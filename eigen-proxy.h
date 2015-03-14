7ef3011a1879b46bbe0987b1354c728c311b242e
Oleg Sidorkin <oleg.sidorkin@gmail.com>
2015-03-14 05:59:27 -0700

    Eigen C++ library updates to version 3.2.4

--- eigen-proxy.h
+++ eigen-proxy.h
@@ -1,5 +1,6 @@
 #define EIGEN_MPL2_ONLY
 #define EIGEN2_SUPPORT
+#define EIGEN_NO_EIGEN2_DEPRECATED_WARNING
 #define EIGEN_INITIALIZE_MATRICES_BY_ZERO
 void eigen_assert_fail(const char* condition, const char* function, const char* file, int line);
 #define eigen_assert(x) do {\
